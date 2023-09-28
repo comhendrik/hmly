3c2ad163cba6f316a29da717df6b77ec241bd36f
comhendrik <hendrik0807@icloud.com>
2023-09-28 15:01:21 +0200

    Refactored login and signup widget

--- lib/features/authentication/presentation/widgets/authentication.dart
+++ lib/features/authentication/presentation/widgets/authentication.dart
@@ -4,6 +4,8 @@ import 'package:household_organizer/features/authentication/presentation/bloc/au
 
 
 class AuthenticationWidget extends StatefulWidget {
+
+
   const AuthenticationWidget({
     super.key
   });
@@ -17,123 +19,6 @@ class AuthenticationWidget extends StatefulWidget {
 
 class _AuthenticationWidget extends State<AuthenticationWidget> {
   bool showLogin = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: ListView(
-        children: [
-          const Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.house, weight: 5.0),
-              Text(' Welcome', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
-            ],
-          ),
-          const SizedBox(height: 5.0,),
-          Text(
-            showLogin ? 'Login': 'SignUp',
-            style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          ),
-          getLoginOrSignUp(showLogin),
-          ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  showLogin = !showLogin;
-                });
-              },
-              child: showLogin ? const Text("No account? Register Now!") : const Text("Already registered? Login!")
-          ),
-        ],
-      ),
-    );
-  }
-
-
-
-}
-
-Widget getLoginOrSignUp(bool showLogin) {
-  if (showLogin)  {
-    return const LoginView();
-  }
-  return const SignUpView();
-}
-
-class LoginView extends StatefulWidget {
-  const LoginView({super.key});
-
-
-  @override
-  State<LoginView> createState() => _LoginView();
-}
-
-class _LoginView extends State<LoginView> {
-
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  String emailStr = '';
-  String passwordStr = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          TextField(
-              controller: emailController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'E-Mail',
-                hintText: 'Enter your e-mail address',
-                prefixIcon: Icon(Icons.person), // Icon for username
-              ),
-              onChanged: (value) {
-                emailStr = value;
-              }
-          ),
-          TextField(
-              controller: passwordController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your password',
-                prefixIcon: Icon(Icons.lock), // Icon for password
-              ),
-              onChanged: (value) {
-                passwordStr = value;
-              }
-          ),
-          ElevatedButton.icon(
-              onPressed: () {
-                login(emailStr, passwordStr);
-              },
-              icon: const Icon(Icons.arrow_forward),
-              label: const Text("Login")
-          )
-        ],
-      ),
-    );
-  }
-
-  void login(String email, String password) {
-    BlocProvider.of<AuthBloc>(context)
-        .add(CreateAuthEvent(email: email, password: password));
-  }
-
-}
-
-class SignUpView extends StatefulWidget {
-  const SignUpView({super.key});
-
-
-  @override
-  State<SignUpView> createState() => _SignUpView();
-}
-
-class _SignUpView extends State<SignUpView> {
-
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final passwordConfirmController = TextEditingController();
@@ -144,88 +29,128 @@ class _SignUpView extends State<SignUpView> {
   String passwordConfirmStr = '';
   String usernameStr = '';
   String nameStr = '';
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          TextField(
-              controller: emailController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'E-Mail',
-                hintText: 'Enter your email address',
-                prefixIcon: Icon(Icons.email), // Icon for password
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            const Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.house, weight: 5.0),
+                Text(' Welcome', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
+              ],
+            ),
+            const SizedBox(height: 5.0,),
+            Text(
+              showLogin ? 'Login': 'SignUp',
+              style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+                controller: emailController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  labelText: 'E-Mail',
+                  hintText: 'Enter your e-mail address',
+                  prefixIcon: Icon(Icons.person), // Icon for username
+                ),
+                onChanged: (value) {
+                  emailStr = value;
+                }
+            ),
+            TextField(
+                controller: passwordController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  prefixIcon: Icon(Icons.lock), // Icon for password
+                ),
+                onChanged: (value) {
+                  passwordStr = value;
+                }
+            ),
+            if (!showLogin)
+              Column(
+                children: [
+                  TextField(
+                      controller: passwordConfirmController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: 'Confirm Password',
+                        hintText: 'Re-enter your password',
+                        prefixIcon: Icon(Icons.lock), // Icon for password
+                      ),
+                      onChanged: (value) {
+                        passwordConfirmStr = value;
+                      }
+                  ),
+                  TextField(
+                      controller: usernameController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                        hintText: 'Enter your username',
+                        prefixIcon: Icon(Icons.person), // Icon for password
+                      ),
+                      onChanged: (value) {
+                        usernameStr = value;
+                      }
+                  ),
+                  TextField(
+                      controller: nameController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                        labelText: 'Name',
+                        hintText: 'Enter your name',
+                        prefixIcon: Icon(Icons.badge), // Icon for password
+                      ),
+                      onChanged: (value) {
+                        nameStr = value;
+                      }
+                  ),
+                ],
               ),
-              onChanged: (value) {
-                emailStr = value;
-              }
-          ),
-          TextField(
-              controller: passwordController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your password',
-                prefixIcon: Icon(Icons.lock), // Icon for password
-              ),
-              onChanged: (value) {
-                passwordStr = value;
-              }
-          ),
-          TextField(
-              controller: passwordConfirmController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'Confirm Password',
-                hintText: 'Re-enter your password',
-                prefixIcon: Icon(Icons.lock), // Icon for password
-              ),
-              onChanged: (value) {
-                passwordConfirmStr = value;
-              }
-          ),
-          TextField(
-              controller: usernameController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                hintText: 'Enter your username',
-                prefixIcon: Icon(Icons.person), // Icon for password
-              ),
-              onChanged: (value) {
-                usernameStr = value;
-              }
-          ),
-          TextField(
-              controller: nameController,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'Name',
-                hintText: 'Enter your name',
-                prefixIcon: Icon(Icons.badge), // Icon for password
-              ),
-              onChanged: (value) {
-                nameStr = value;
-              }
-          ),
-          ElevatedButton.icon(
-              onPressed: () {
-                signUp(emailStr, passwordStr, passwordConfirmStr, usernameStr, nameStr);
-              },
-              icon: const Icon(Icons.arrow_forward),
-              label: const Text("Sign Up")
-          )
-        ],
-      ),
+            ElevatedButton.icon(
+                onPressed: () {
+                  if (showLogin) {
+                    login(emailStr, passwordStr);
+                  } else {
+                    signUp(emailStr, passwordStr, passwordConfirmStr, usernameStr, nameStr);
+                  }
+                },
+                icon: const Icon(Icons.arrow_forward),
+                label: Text(showLogin ? "Login" : "SignUp")
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showLogin = !showLogin;
+                  });
+                },
+                child: showLogin ? const Text("No account? Register Now!") : const Text("Already registered? Login!")
+            ),
+          ],
+        ),
+      )
     );
+  }
+
+  void login(String email, String password) {
+    BlocProvider.of<AuthBloc>(context)
+        .add(CreateAuthEvent(email: email, password: password));
   }
 
   void signUp(String email, String password, String passwordConfirm, String username, String name) {
     BlocProvider.of<AuthBloc>(context)
         .add(CreateAuthDataOnServerEvent(email: email, password: password, passwordConfirm: passwordConfirm, username: username, name: name));
   }
-
 }
+
+
 
