8317e8813b034d14935f69c4e720f809e86230b9
comhendrik <hendrik0807@icloud.com>
2023-11-10 17:25:02 +0100

    added tests for usecase in household feature

--- test/features/household/domain/usecases/update_admin_test.dart
+++ test/features/household/domain/usecases/update_admin_test.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:household_organizer/core/error/failure.dart';
 import 'package:household_organizer/features/household/domain/repositories/household_repository.dart';
-import 'package:household_organizer/features/household/domain/usecases/load_household.dart';
+import 'package:household_organizer/features/household/domain/usecases/update_admin.dart';
 import 'package:mocktail/mocktail.dart';
 import '../../../../test_data.dart';
 
@@ -10,33 +10,33 @@ class MockHouseholdRepository extends Mock implements HouseholdRepository {}
 
 void main() {
   late MockHouseholdRepository repository;
-  late LoadHousehold usecase;
+  late UpdateAdmin usecase;
 
   setUpAll(() {
     repository = MockHouseholdRepository();
-    usecase = LoadHousehold(repository: repository);
+    usecase = UpdateAdmin(repository: repository);
   });
 
   test('should get valid object when call is successful', () async {
-    when(() => repository.loadHousehold("householdID")).thenAnswer((_) async => Right(tHousehold));
+    when(() => repository.updateAdmin("householdID", "userID")).thenAnswer((_) async => Right(tHousehold));
 
-    final result = await usecase.execute("householdID");
+    final result = await usecase.execute("householdID", "userID");
 
     expect(result, equals(Right(tHousehold)));
   });
 
   test('should get Failure of FailureType.server when when call is unsuccessful and throws Server Exception', () async {
-    when(() => repository.loadHousehold("householdID")).thenAnswer((_) async => Left(Failure(data: tFailureTypeServerResponseStructure, type: FailureType.server)));
+    when(() => repository.updateAdmin("householdID", "userID")).thenAnswer((_) async => Left(Failure(data: tFailureTypeServerResponseStructure, type: FailureType.server)));
 
-    final result = await usecase.execute("householdID");
+    final result = await usecase.execute("householdID", "userID");
 
     expect(result, equals(Left(Failure(data: tFailureTypeServerResponseStructure, type: FailureType.server))));
   });
 
   test('should get Failure of FailureType.unknown when when call is unsuccessful and throws Unknown Exception', () async {
-    when(() => repository.loadHousehold("householdID")).thenAnswer((_) async => Left(Failure(data: tFailureTypeUnknownResponseStructure, type: FailureType.unknown)));
+    when(() => repository.updateAdmin("householdID", "userID")).thenAnswer((_) async => Left(Failure(data: tFailureTypeUnknownResponseStructure, type: FailureType.unknown)));
 
-    final result = await usecase.execute("householdID");
+    final result = await usecase.execute("householdID", "userID");
 
     expect(result, equals(Left(Failure(data: tFailureTypeUnknownResponseStructure, type: FailureType.unknown))));
   });
