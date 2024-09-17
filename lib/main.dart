2f9de83e79ea0e70724694571f51cbe032b7272b
comhendrik <76622601+comhendrik@users.noreply.github.com>
2024-09-17 21:28:15 +0200

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:hmly/features/authentication/presentation/pages/auth_page.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'injection_container.dart' as di;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
